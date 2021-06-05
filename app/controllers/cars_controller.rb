6765e44dbc17580f7d55bb3f62e2cfcc3df1b80f
Christerper <christerper91@gmail.com>
2021-06-05 21:00:18 +0800

    add car detail form to receive id from supplier

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,49 @@
 class CarsController < ApplicationController
+  def index
+    @cars = Car.all
+  end
+
+  def show
+    @car = Car.find(params[:id])
+    @detail = @car.booking.customer.supplier
+    @days = (@detail.booking.end_date - @detail.booking.start_date).floor/(60*60*24)
+  end
+
+  def new
+    @car = Car.new(customer_id: params[:customer_id], booking_id: params[:booking_id], supplier_id: params[:supplier_id])
+  end
+
+  def edit
+    @car = Car.find(params[:id])
+  end
+
+  def create
+    @car = current_user.cars.build(car_params)
+    if @car.save
+      redirect_to @car, notice: "car was successfully created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @cars = Car.find(params[:id])
+    @cars.update(car_params)
+    if @cars.save
+      redirect_to @cars, notice: "Car was successfully Updated"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @cars = Car.find(params[:id])
+    @cars.destroy
+
+  end
+
+  private
+  def car_params
+    params.require(:car).permit(:type_of_car, :car_plate_no, :car_agent_rate, :car_selling_rate, :customer_id, :booking_id, :supplier_id)
+  end
 end
