d63867e8cd0b7018be8d8157dc1303ac92e84422
Christerper <christerper91@gmail.com>
2021-06-12 18:06:47 +0800

    change smtp email to sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -7,9 +7,9 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :port => 587,
-  :address => 'smtp.mailgun.org',
-  :user_name => Rails.application.credentials.dig(Rails.env.to_sym,:smtp, :user_name), # This is the string literal 'apikey', NOT the ID of your API key
-  :password => Rails.application.credentials.dig(Rails.env.to_sym,:smtp, :password), # This is the secret sendgrid API key which was issued during API key creation
+  :address => 'smtp.sendgrid.net',
+  :user_name => Rails.application.credentials.dig(Rails.env.to_sym,:smtpsg, :user_name), # This is the string literal 'apikey', NOT the ID of your API key
+  :password => Rails.application.credentials.dig(Rails.env.to_sym,:smtpsg, :password), # This is the secret sendgrid API key which was issued during API key creation
   :domain => 'https://car-rento-ibm.herokuapp.com/',
   :authentication => :plain,
   # :enable_starttls_auto => true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,9 +102,9 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :port => 587,
-    :address => 'smtp.mailgun.org',
-    :user_name => Rails.application.credentials.dig(Rails.env.to_sym,:smtp, :user_name), # This is the string literal 'apikey', NOT the ID of your API key
-    :password => Rails.application.credentials.dig(Rails.env.to_sym,:smtp, :password), # This is the secret sendgrid API key which was issued during API key creation
+    :address => 'smtp.sendgrid.net',
+    :user_name => Rails.application.credentials.dig(Rails.env.to_sym,:smtpsg, :user_name), # This is the string literal 'apikey', NOT the ID of your API key
+    :password => Rails.application.credentials.dig(Rails.env.to_sym,:smtpsg, :password), # This is the secret sendgrid API key which was issued during API key creation
     :domain => 'https://car-rento-ibm.herokuapp.com/',
     :authentication => :plain,
     :enable_starttls_auto => true
